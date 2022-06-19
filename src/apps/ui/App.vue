b93b99ce17220ba6e272468d7f66916a7a02e704
wajeht <58354193+wajeht@users.noreply.github.com>
2022-06-19 15:00:47 -0400

    feat: todo exercise

--- src/apps/ui/App.vue
+++ src/apps/ui/App.vue
@@ -1,25 +1,53 @@
-<script setup>
-  // This starter template is using Vue 3 <script setup> SFCs
-  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-  import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-  <div id="testDiv">THIS IS A TEST</div>
+  <div v-for="item in todos" :key="item.id">
+    <div id="todo-item">
+      <p>{{ item.name }}</p>
+      <button @click="deleteTodo(item.id)">🗑</button>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          name: 'walk the dog',
+          isDone: false,
+        },
+        {
+          id: 2,
+          name: 'shower',
+          isDone: false,
+        },
+        {
+          id: 4,
+          name: 'eat',
+          isDone: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    addTodo() {},
+    deleteTodo(given_id) {
+      this.todos = this.todos.filter((item) => item.id != given_id);
+    },
+  },
+};
+</script>
+
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  #testDiv{
-    background-color: red;
-  }
+#todo-item {
+  display: flex;
+  justify-content: space-between;
+
+  color: white;
+  background: gray;
+  padding: 10px;
+  border-radius: 5px;
+  margin-top: 5px;
+}
 </style>
