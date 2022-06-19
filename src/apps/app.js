fd4ea0f6727ca89254ea118996ec3865e86c4556
Jaw <58354193+wajeht@users.noreply.github.com>
2022-06-19 06:47:25 -0400

    chore: scaffold project files

--- src/apps/app.js
+++ src/apps/app.js
@@ -1,18 +1,30 @@
-import path from "path";
+import path from 'path';
+import compression from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
 
-import express from "express";
+import express from 'express';
 const app = express();
 
 const PORT = 8080;
-import api from "./api/api.js";
+import api from './api/api.js';
 
+// TODO!: configure this helmet for production
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  }),
+);
+app.use(cors({ origin: '*' }));
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(path.join(process.cwd(), "src", "public")))); // prettier-ignore
 
-app.use("/api", api);
+app.use('/api', api);
 
-app.use("*", (req, res, next) => {
+app.use('*', (req, res, next) => {
   try {
     res.sendFile(path.resolve(path.join(process.cwd(), "src", "public", "index.html"))); // prettier-ignore
   } catch (error) {
