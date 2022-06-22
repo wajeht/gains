deda6ddb0a4705810d957fbd36fd4ece529f0e24
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2022-06-21 19:35:58 -0500

    Update App.vue

--- src/apps/ui/App.vue
+++ src/apps/ui/App.vue
@@ -27,9 +27,10 @@
           <span id="id">{{ item.id }}</span>
           <span id="taskName">>{{ item.name }}</span>
           <p :class="[`obj-${item.isDone}`]">COMPLETE:{{ item.isDone }}</p>
+          <span>{{ item.modifiedDate }}</span>
         </div>
 
-        <!-- mark done button -->
+        <!-- mark done button taskName-->
         <button class="btn btn-sm btn-success" @click="completeToDo(item.id)">
           <font-awesome-icon icon="bug" />
           Mark Done
@@ -46,83 +47,92 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        todoItem: '',
-        todos: [
-          {
-            id: 1,
-            name: 'walk the dog',
-            isDone: false,
-          },
-          {
-            id: 2,
-            name: 'shower',
-            isDone: false,
-          },
-          {
-            id: 4,
-            name: 'eat',
-            isDone: true,
-          },
-        ],
-      };
-    },
-    methods: {
-      addTodo(todoItem) {
-        this.todos.push({
-          id: Math.floor(Math.random() * 999999999999),
-          name: todoItem,
+export default {
+  data() {
+    return {
+      todoItem: '',
+      todos: [
+        {
+          id: 1,
+          name: 'walk the dog',
           isDone: false,
-        });
-      },
-      deleteTodo(given_id) {
-        this.todos = this.todos.filter((item) => item.id != given_id);
-      },
-      completeToDo(given_id) {
-        this.todos[this.todos.findIndex((item) => item.id == given_id)].isDone == true
-          ? (this.todos[this.todos.findIndex((item) => item.id == given_id)].isDone = false)
-          : (this.todos[this.todos.findIndex((item) => item.id == given_id)].isDone = true);
-      },
-      clearAll() {
-        this.todos = [];
-      },
+          postedDate: '6/21/2022, 7:17:06 PM',
+          modifiedDate: '6/21/2022, 7:17:06 PM',
+        },
+        {
+          id: 2,
+          name: 'shower',
+          isDone: false,
+          postedDate: '6/21/2022, 7:17:06 PM',
+          modifiedDate: '6/21/2022, 7:17:06 PM',
+        },
+        {
+          id: 4,
+          name: 'eat',
+          isDone: true,
+          postedDate: '6/21/2022, 7:17:06 PM',
+          modifiedDate: '6/21/2022, 7:17:06 PM',
+        },
+      ],
+    };
+  },
+  methods: {
+    addTodo(todoItem) {
+      this.todos.push({
+        id: Math.floor(Math.random() * 999999999999),
+        name: todoItem,
+        isDone: false,
+        postedDate: new Date().toLocaleString('en-US'),
+        modifiedDate: new Date().toLocaleString('en-US'),
+      });
     },
-  };
+    deleteTodo(given_id) {
+      this.todos = this.todos.filter((item) => item.id != given_id);
+    },
+    completeToDo(given_id) {
+      this.todos[this.todos.findIndex((item) => item.id == given_id)].isDone == true
+        ? (this.todos[this.todos.findIndex((item) => item.id == given_id)].isDone = false)
+        : (this.todos[this.todos.findIndex((item) => item.id == given_id)].isDone = true);
+      this.todos[this.todos.findIndex((item) => item.id == given_id)].modifiedDate = new Date().toLocaleString('en-US');
+    },
+    clearAll() {
+      this.todos = [];
+    },
+  },
+};
 </script>
 
 <style>
-  #todo-item {
-    display: flex;
-    justify-content: space-between;
+#todo-item {
+  display: flex;
+  justify-content: space-between;
 
-    color: white;
-    background: gray;
-    padding: 10px;
-    border-radius: 5px;
-    margin-top: 5px;
-  }
-  #item {
-    display: flex;
-  }
-  #taskName {
-    margin-right: 10px;
-  }
-  #id {
-    margin-right: 10px;
-  }
-  .obj-false {
-    background-color: red;
-  }
-  .obj-true {
-    background-color: rgb(35, 216, 29);
-    margin-left: 100px;
-  }
-  #addToDoBox {
-    background-color: lightblue;
-  }
-  #addToDoButton {
-    background-color: rgb(32, 150, 77);
-  }
+  color: white;
+  background: gray;
+  padding: 10px;
+  border-radius: 5px;
+  margin-top: 5px;
+}
+#item {
+  display: flex;
+}
+#taskName {
+  margin-right: 10px;
+}
+#id {
+  margin-right: 10px;
+}
+.obj-false {
+  background-color: red;
+}
+.obj-true {
+  background-color: rgb(35, 216, 29);
+  margin-left: 100px;
+}
+#addToDoBox {
+  background-color: lightblue;
+}
+#addToDoButton {
+  background-color: rgb(32, 150, 77);
+}
 </style>
