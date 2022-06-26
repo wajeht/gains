4a401979a5e88c1d29e0c74b4cd162e733933519
wajeht <58354193+wajeht@users.noreply.github.com>
2022-06-26 19:25:43 -0400

    refactor: only send chad msg on production enviroment

--- src/libs/chad.js
+++ src/libs/chad.js
@@ -1,5 +1,6 @@
 import logger from './logger.js';
-import { discord } from '../config/env.js';
+import { discord, env } from '../config/env.js';
+
 import axios from 'axios';
 
 /* It sends a message to a Discord channel */
@@ -10,10 +11,7 @@ export default class Chad {
       let params = null;
 
       if (object == null) {
-        params = {
-          username: 'Chad',
-          content: msg,
-        };
+        params = { username: 'Chad', content: msg };
       } else {
         params = {
           username: 'Chad',
@@ -27,16 +25,16 @@ export default class Chad {
         };
       }
 
-      const res = await axios({
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        data: JSON.stringify(params),
-        url: discord.url,
-      });
+      let res = null;
 
-      if (res.status === 204) logger.info(`Chad sent ${msg}`);
+      // only send chad message to discord in production environment
+      // prettier-ignore
+      if (env === 'production') {
+        res = await axios({ method: 'POST', headers: { 'Content-Type': 'application/json', }, data: JSON.stringify(params), url: discord.url });
+        if (res?.status === 204) logger.info(`Chad sent ${msg}`);
+      } else  {
+        logger.warn(`Skipping Chad message in dev environment!`)
+      }
     } catch (e) {
       logger.error(e);
     }
