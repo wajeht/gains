523d1ea197ee95cc2a64964cf7260ee3c52cc770
wajeht <58354193+wajeht@users.noreply.github.com>
2022-06-25 23:25:06 -0400

    feat: added stuff to home componnet

--- src/apps/ui/pages/regular/RegularHome.vue
+++ src/apps/ui/pages/regular/RegularHome.vue
@@ -1,29 +1,106 @@
 <template>
-  <div class="d-flex align-items-center justify-content-center" style="height: calc(100vh - 498px)">
-    <div class="text-center">
-      <h1 class="display-4 fw-bold">Let's make all kinds of gains!</h1>
-      <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4">
-          Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s
-          most popular front-end open source toolkit, featuring Sass variables and mixins,
-          responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.
-        </p>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-          <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Sign up</button>
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Learn more</button>
-        </div>
+  <div class="text-center px-4 py-5 border-bottom">
+    <h1 class="display-4 fw-bold">Let's make all kinds of gains!</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">
+        Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
+        popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid
+        system, extensive prebuilt components, and powerful JavaScript plugins.
+      </p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+        <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Sign up</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Learn more</button>
       </div>
-      <div class="overflow-hidden" style="max-height: 30vh">
-        <div class="container px-5">
-          <img
-            src="../../assets/images/hero.png"
-            class="img-fluid rounded-3 mb-4"
-            alt="gains app screenshot"
-            width="700"
-            height="500"
-            loading="lazy"
-          />
+    </div>
+    <div class="overflow-hidden" style="max-height: 30vh">
+      <div class="container px-5">
+        <img
+          src="../../assets/images/hero.png"
+          class="img-fluid rounded-3 mb-4"
+          alt="gains app screenshot"
+          width="700"
+          height="500"
+          loading="lazy"
+        />
+      </div>
+    </div>
+  </div>
+
+  <div class="px-4 py-5">
+    <div class="row g-4 row-cols-1 row-cols-lg-3">
+      <!-- easy -->
+      <div class="feature col">
+        <div class="p-2 d-flex justify-content-center align-items-center">
+          <i class="bi bi-hand-index-thumb" style="font-size: 5em"></i>
         </div>
+        <h2 class="text-center">Easy to use</h2>
+        <p>
+          Sign up using your fake information to start uploading videos. No complicated setup
+          required!
+        </p>
+      </div>
+
+      <!-- open source -->
+      <div class="feature col">
+        <div class="p-2 d-flex justify-content-center align-items-center">
+          <i class="bi bi-github" style="font-size: 5em"></i>
+        </div>
+        <h2 class="text-center">Open source</h2>
+        <p>
+          This full-stack web app is open sourced on Github. You can go check it out via this
+          <a href="https://github.com/allkindsofgains/gains" target="_blank">link!</a>
+        </p>
+      </div>
+
+      <!-- privacy -->
+      <div class="feature col">
+        <div class="p-2 d-flex justify-content-center align-items-center">
+          <i class="bi bi-shield-check" style="font-size: 5em"></i>
+        </div>
+        <h2 class="text-center">Keep your privacy</h2>
+        <p>
+          We do not collect any of your data to do anything else than just logging your videos.
+          Nothing more, nothing less!
+        </p>
+      </div>
+
+      <!-- change this -->
+      <div class="feature col">
+        <div class="p-2 d-flex justify-content-center align-items-center">
+          <i class="bi bi-shield-check" style="font-size: 5em"></i>
+        </div>
+        <h2 class="text-center">change this</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus nesciunt fugit delectus
+          est, velit ipsum eius porro cupiditate earum doloribus odit explicabo eligendi aliquam
+          animi. Neque repellendus tenetur assumenda est!
+        </p>
+      </div>
+
+      <!-- change this -->
+      <div class="feature col">
+        <div class="p-2 d-flex justify-content-center align-items-center">
+          <i class="bi bi-shield-check" style="font-size: 5em"></i>
+        </div>
+        <h2 class="text-center">change this</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptate suscipit numquam
+          eaque sunt blanditiis? A eligendi sapiente sed consectetur. Corrupti eum saepe
+          perspiciatis iure. Esse officia earum fugiat totam.
+        </p>
+      </div>
+
+      <!-- change this -->
+      <div class="feature col">
+        <div class="p-2 d-flex justify-content-center align-items-center">
+          <i class="bi bi-shield-check" style="font-size: 5em"></i>
+        </div>
+        <h2 class="text-center">change this</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quidem iste
+          mollitia autem doloremque ipsum voluptas explicabo odit eius at laudantium expedita dicta
+          nesciunt, temporibus fugit aspernatur veritatis cum.
+        </p>
       </div>
     </div>
   </div>
