ed59ae4dc53d8d7db0b33056f58563a2bbc4c325
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2022-06-30 20:53:16 -0500

    feature suggestions (#29)
    feature suggestions

--- src/apps/ui/pages/regular/Features.vue
+++ src/apps/ui/pages/regular/Features.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>Features</h1>
+  <ul>
+    <li>rep/weight tracker</li>
+    <li>lift tracker</li>
+    <li>calender</li>
+    <li>1rm calc</li>
+    <li>ranking system??</li>
+    <li>over time tracking 1m 1y 5y</li>
+  </ul>
 </template>
