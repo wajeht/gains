94d73286d1725632b4c7e1e463f52f8b8b82ed8b
wajeht <58354193+wajeht@users.noreply.github.com>
2022-07-01 09:49:40 -0400

    feat: added o-auth logins btns on login page

--- src/apps/ui/pages/regular/Login.vue
+++ src/apps/ui/pages/regular/Login.vue
@@ -1,6 +1,5 @@
 <template>
-  <form
-    @submit.prevent="handleSubmit"
+  <div
     class="
       px-4
       col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4
@@ -8,69 +7,92 @@
       animate__animated animate__fadeIn
     "
   >
-    <!-- title -->
-    <h1 class="mb-3">Login</h1>
+    <!-- form -->
+    <form @submit.prevent="handleSubmit">
+      <!-- title -->
+      <h1 class="mb-3">Login</h1>
 
-    <!-- error -->
-    <div v-if="error.length" class="mb-3 alert alert-danger">
-      <span>{{ error }}</span>
-    </div>
-
-    <!-- email -->
-    <div class="mb-3">
-      <label for="email" class="form-label">Email address</label>
-      <input
-        v-model="email"
-        type="email"
-        class="form-control"
-        id="email"
-        placeholder="test@test.com"
-        required
-        :disabled="loading"
-      />
-    </div>
-
-    <!-- password -->
-    <div class="mb-3">
-      <label for="password" class="form-label">Password</label>
-      <input
-        v-model="password"
-        type="password"
-        class="form-control"
-        id="password"
-        placeholder="test"
-        required
-        :disabled="loading"
-      />
-    </div>
-
-    <!-- checkbox -->
-    <div class="mb-3 form-check">
-      <input type="checkbox" class="form-check-input" id="checkbox" :disabled="loading" />
-      <div class="d-flex justify-content-between">
-        <label class="form-check-label" for="checkbox-label">Remember me</label>
-        <router-link class="text-decoration-none link-dark" to="/forget-password"
-          >Forget password?</router-link
-        >
-      </div>
-    </div>
-
-    <!-- button -->
-    <button type="submit" class="btn btn-dark" :disabled="loading">
-      <div v-if="loading" class="spinner-border spinner-border-sm" role="status">
-        <span class="visually-hidden">Loading...</span>
+      <!-- error -->
+      <div v-if="error.length" class="mb-3 alert alert-danger">
+        <span>{{ error }}</span>
       </div>
 
-      <span v-if="!loading"> Submit </span>
-      <span v-if="loading"> Loading... </span>
-    </button>
-  </form>
+      <!-- email -->
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input
+          v-model="email"
+          type="email"
+          class="form-control"
+          id="email"
+          placeholder="test@test.com"
+          required
+          :disabled="loading"
+        />
+      </div>
+
+      <!-- password -->
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input
+          v-model="password"
+          type="password"
+          class="form-control"
+          id="password"
+          placeholder="test"
+          required
+          :disabled="loading"
+        />
+      </div>
+
+      <!-- checkbox -->
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="checkbox" :disabled="loading" />
+        <div class="d-flex justify-content-between">
+          <label class="form-check-label" for="checkbox-label">Remember me</label>
+          <router-link class="text-decoration-none link-dark" to="/forget-password"
+            >Forget password?</router-link
+          >
+        </div>
+      </div>
+
+      <!-- button -->
+      <button type="submit" class="btn btn-dark" :disabled="loading">
+        <div v-if="loading" class="spinner-border spinner-border-sm" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+
+        <span v-if="!loading"> Submit </span>
+        <span v-if="loading"> Loading... </span>
+      </button>
+    </form>
+
+    <!-- or -->
+    <Or />
+
+    <!-- o-auth -->
+    <div class="d-flex flex-column gap-2">
+      <!-- discord -->
+      <a class="btn w-100 text-light" href="#" style="background: #7289da"
+        ><i class="bi bi-discord me-1"></i>Login with Discord</a
+      >
+
+      <!-- github -->
+      <a class="btn btn-light w-100" style="border: 1px solid #ced4da" href="#"
+        ><i class="bi bi-github me-1"></i>Login with Github</a
+      >
+    </div>
+  </div>
 </template>
 
 <script>
   import { sleep } from '../../../../utils/helpers.js';
+  import Or from '../../components/shared/Or.vue';
 
   export default {
+    components: {
+      Or,
+    },
     data() {
       return {
         email: '',
