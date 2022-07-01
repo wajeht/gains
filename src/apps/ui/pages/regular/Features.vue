75a752e1f6b6aa1f0379b15f4c386d3ef516502f
wajeht <58354193+wajeht@users.noreply.github.com>
2022-07-01 09:49:22 -0400

    feat: added contribute cta in features page

--- src/apps/ui/pages/regular/Features.vue
+++ src/apps/ui/pages/regular/Features.vue
@@ -1,133 +1,148 @@
 <template>
   <div class="px-4 mx-auto animate__animated animate__fadeIn">
+    <!-- github -->
+    <section class="pb-5 text-center">
+      <!-- title -->
+      <h2 class="featurette-heading">
+        Are you a developer? <span class="text-muted">That's cool..!</span>
+      </h2>
+
+      <!-- description -->
+      <p class="lead py-3">
+        Help us bring new ideas to life by contributing to our long term project.
+      </p>
+
+      <!-- button -->
+      <a
+        class="btn btn-success w-50"
+        href="https://github.com/allkindsofgains/gains"
+        target="_blank"
+      >
+        <i class="bi bi-github me-1"></i>
+        Contribute
+      </a>
+    </section>
+
     <!-- feature 1 -->
-    <section>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">
-              First featurette heading. <span class="text-muted">It’ll blow your mind.</span>
-            </h2>
-            <p class="lead">
-              Some great placeholder content for the first featurette here. Imagine some exciting
-              prose here.
-            </p>
-          </div>
-          <div class="col-md-5">
-            <svg
-              class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-              width="500"
-              height="500"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: 500x500"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#eee"></rect>
-              <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-            </svg>
-          </div>
+    <section class="py-5 border-top">
+      <div class="row">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">
+            First featurette heading. <span class="text-muted">It’ll blow your mind.</span>
+          </h2>
+          <p class="lead">
+            Some great placeholder content for the first featurette here. Imagine some exciting
+            prose here.
+          </p>
+        </div>
+        <div class="col-md-5">
+          <svg
+            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="500"
+            height="500"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: 500x500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#eee"></rect>
+            <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+          </svg>
         </div>
       </div>
     </section>
 
     <!-- feature 2 -->
     <section class="bg-white py-5 border-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">
-              Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span>
-            </h2>
-            <p class="lead">
-              Another featurette? Of course. More placeholder content here to give you an idea of
-              how this layout would work with some actual real-world content in place.
-            </p>
-          </div>
-          <div class="col-md-5 order-md-1">
-            <svg
-              class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-              width="500"
-              height="500"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: 500x500"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#eee"></rect>
-              <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-            </svg>
-          </div>
+      <div class="row">
+        <div class="col-md-7 order-md-2">
+          <h2 class="featurette-heading">
+            Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span>
+          </h2>
+          <p class="lead">
+            Another featurette? Of course. More placeholder content here to give you an idea of how
+            this layout would work with some actual real-world content in place.
+          </p>
+        </div>
+        <div class="col-md-5 order-md-1">
+          <svg
+            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="500"
+            height="500"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: 500x500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#eee"></rect>
+            <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+          </svg>
         </div>
       </div>
     </section>
 
     <!-- feature 3 -->
     <section class="py-5 border-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">
-              First featurette heading. <span class="text-muted">It’ll blow your mind.</span>
-            </h2>
-            <p class="lead">
-              Some great placeholder content for the first featurette here. Imagine some exciting
-              prose here.
-            </p>
-          </div>
-          <div class="col-md-5">
-            <svg
-              class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-              width="500"
-              height="500"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: 500x500"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#eee"></rect>
-              <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-            </svg>
-          </div>
+      <div class="row">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">
+            First featurette heading. <span class="text-muted">It’ll blow your mind.</span>
+          </h2>
+          <p class="lead">
+            Some great placeholder content for the first featurette here. Imagine some exciting
+            prose here.
+          </p>
+        </div>
+        <div class="col-md-5">
+          <svg
+            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="500"
+            height="500"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: 500x500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#eee"></rect>
+            <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+          </svg>
         </div>
       </div>
     </section>
 
     <!-- feature 4 -->
     <section class="px-4 py-5 border-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">
-              Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span>
-            </h2>
-            <p class="lead">
-              Another featurette? Of course. More placeholder content here to give you an idea of
-              how this layout would work with some actual real-world content in place.
-            </p>
-          </div>
-          <div class="col-md-5 order-md-1">
-            <svg
-              class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-              width="500"
-              height="500"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: 500x500"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#eee"></rect>
-              <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-            </svg>
-          </div>
+      <div class="row">
+        <div class="col-md-7 order-md-2">
+          <h2 class="featurette-heading">
+            Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span>
+          </h2>
+          <p class="lead">
+            Another featurette? Of course. More placeholder content here to give you an idea of how
+            this layout would work with some actual real-world content in place.
+          </p>
+        </div>
+        <div class="col-md-5 order-md-1">
+          <svg
+            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="500"
+            height="500"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: 500x500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#eee"></rect>
+            <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+          </svg>
         </div>
       </div>
     </section>
