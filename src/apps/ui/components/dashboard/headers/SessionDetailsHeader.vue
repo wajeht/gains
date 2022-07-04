34f97423230bf9279a4e866261f438602f0b8e7c
wajeht <58354193+wajeht@users.noreply.github.com>
2022-07-03 20:26:28 -0400

    refactor: change setting header to use icons

--- src/apps/ui/components/dashboard/headers/SessionDetailsHeader.vue
+++ src/apps/ui/components/dashboard/headers/SessionDetailsHeader.vue
@@ -14,19 +14,42 @@
     "
   >
     <!-- back -->
-    <button @click="$router.back()" class="btn btn-outline-dark">
-      <font-awesome-icon icon="angle-left" class="p-0 m-0" />
-      {{ previousPageName }}
-    </button>
+    <span role="button" @click="$router.back()" class="link-secondary">
+      <h5 class="m-0 p-0 d-flex justify-content-center align-items-center gap-2">
+        <font-awesome-icon icon="angle-left" class="p-0 m-0" />
+        {{ previousPageName }}
+      </h5>
+    </span>
 
     <!-- middle -->
     <!-- <input type="text" class="form-control form-control-sm" id="search" placeholder="Search.." /> -->
 
     <!-- Edit -->
-    <button class="btn btn-outline-dark">
+    <!-- <button class="btn btn-outline-dark">
       <i class="bi bi-pencil-square"></i>
       Edit
-    </button>
+    </button> -->
+
+    <div class="dropdown">
+      <!-- setting icons -->
+      <a
+        class="link-dark"
+        role="button"
+        id="dropdownMenuButton1"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <h5 class="m-0 p-0 d-flex justify-content-center align-items-center gap-2">
+          <i class="bi bi-three-dots-vertical"> </i>
+        </h5>
+      </a>
+
+      <!-- setting links -->
+      <ul class="dropdown-menu dropdown-menu-end shadow-sm" style="min-width: fit-content">
+        <li><button class="dropdown-item btn-sm" type="button">Edit</button></li>
+        <li><button class="dropdown-item btn-sm" type="button">Delete</button></li>
+      </ul>
+    </div>
   </div>
 </template>
 
