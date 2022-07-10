2a42410eadf056ff38a8446678ee61b0656b1288
Jaw <58354193+wajeht@users.noreply.github.com>
2022-07-10 06:00:05 -0400

    feat: disable pull to refresh on mobile

--- src/apps/ui/App.vue
+++ src/apps/ui/App.vue
@@ -2,33 +2,41 @@
   <component :is="layout" />
 </template>
 
-<script >
-  import DashboardLayout from './layouts/DashboardLayout.vue';
-  import EmptyDashboardLayout from './layouts/EmptyDashboardLayout.vue';
-  import RegularLayout from './layouts/RegularLayout.vue';
-  import useUserStore from './store/user.store.js';
+<script>
+import DashboardLayout from './layouts/DashboardLayout.vue';
+import EmptyDashboardLayout from './layouts/EmptyDashboardLayout.vue';
+import RegularLayout from './layouts/RegularLayout.vue';
+import useUserStore from './store/user.store.js';
 
-  export default {
-    components: {
-      DashboardLayout,
-      RegularLayout,
-      EmptyDashboardLayout,
+export default {
+  components: {
+    DashboardLayout,
+    RegularLayout,
+    EmptyDashboardLayout,
+  },
+  data() {
+    return {
+      layout: null,
+    };
+  },
+  watch: {
+    $route(to) {
+      const userStore = useUserStore();
+      // set layout by route meta
+      if (to.meta.layout !== undefined) {
+        this.layout = to.meta.layout;
+      } else {
+        this.layout = 'DashboardLayout'; // this is default layout if route meta is not set
+      }
     },
-    data() {
-      return {
-        layout: null,
-      };
-    },
-    watch: {
-      $route(to) {
-        const userStore = useUserStore();
-        // set layout by route meta
-        if (to.meta.layout !== undefined) {
-          this.layout = to.meta.layout;
-        } else {
-          this.layout = 'DashboardLayout'; // this is default layout if route meta is not set
-        }
-      },
-    },
-  };
+  },
+};
 </script>
+
+<style>
+*,
+body,
+html {
+  overscroll-behavior: contain;
+}
+</style>
