35f8208af7a3f3c5d282f671a05600f3c7e665d4
wajeht <58354193+wajeht@users.noreply.github.com>
2022-07-12 12:45:20 -0400

    feat: sapce out top part of footer on regualr pages

--- src/apps/ui/components/regular/RegularFooter.vue
+++ src/apps/ui/components/regular/RegularFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="py-4 border-top bg-white">
+  <footer class="py-4 border-top bg-white mt-4">
     <div class="container d-flex justify-content-between">
       <!-- copyright -->
       <span class="text-muted">Copyright © 2022 allkindsofgains.app</span>
@@ -7,17 +7,12 @@
       <!-- social links -->
       <div class="d-flex gap-2">
         <!-- github -->
-        <a class="link-secondary" href="https://github.com/allkindsofgains" target="_blank"
-          ><i class="bi bi-github"></i
-        ></a>
+        <a class="link-secondary" href="https://github.com/allkindsofgains" target="_blank"><i
+            class="bi bi-github"></i></a>
 
         <!-- discord -->
-        <a
-          class="link-secondary"
-          href="https://discord.com/channels/988167900420534273/989628732895543296"
-          target="_blank"
-          ><i class="bi bi-discord"></i
-        ></a>
+        <a class="link-secondary" href="https://discord.com/channels/988167900420534273/989628732895543296"
+          target="_blank"><i class="bi bi-discord"></i></a>
       </div>
     </div>
   </footer>
