2591f03a103b5c9cd8bb976802ebf54c5d26c662
Jaw <58354193+wajeht@users.noreply.github.com>
2022-07-17 22:21:35 -0400

    feat: auto migrate database on deployment (#90)

---
+++ src/database/migrations/20220718015554_api_keys.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  // api_keys
+  await knex.schema.raw(`
+    CREATE TABLE IF NOT EXISTS api_keys (
+      id                    SERIAL PRIMARY KEY,
+      key                   VARCHAR(500) NOT NULL UNIQUE,
+      hashed_key            VARCHAR(500) NOT NULL UNIQUE,
+      deleted               BOOLEAN DEFAULT FALSE,
+      user_id               INT REFERENCES users on DELETE CASCADE NOT NULL,
+      created_at            TIMESTAMP NOT NULL DEFAULT NOW(),
+      updated_at            TIMESTAMP NOT NULL DEFAULT NOW()
+    );
+  `);
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.raw(`DROP TABLE IF EXISTS api_keys;`);
+}
---
+++ src/database/migrations/20220718015611_sessions.js
@@ -0,0 +1,34 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  // sessions
+  await knex.schema.raw(`
+    CREATE TABLE IF NOT EXISTS sessions (
+      id                        SERIAL PRIMARY KEY,
+      name                      VARCHAR(500) NOT NULL,
+      block_id                  INT REFERENCES blocks on DELETE CASCADE,
+      start_date                TIMESTAMP NOT NULL DEFAULT NOW(),
+      end_date                  TIMESTAMP DEFAULT NULL,
+      body_weight               INT DEFAULT NULL,
+      hours_of_sleep            INT DEFAULT NULL,
+      caffeine_intake           INT DEFAULT NULL,
+      session_rpe               INT DEFAULT NULL,
+      logs                      jsonb,
+      deleted                   BOOLEAN DEFAULT FALSE,
+      notes                     VARCHAR(1000) DEFAULT NULL,
+      user_id                   INT REFERENCES users on DELETE CASCADE NOT NULL,
+      created_at                TIMESTAMP NOT NULL DEFAULT NOW(),
+      updated_at                TIMESTAMP NOT NULL DEFAULT NOW()
+    );
+  `);
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.raw(`DROP TABLE IF EXISTS sessions;`);
+}
