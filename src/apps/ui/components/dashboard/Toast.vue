38243daf7cf5f88d4f162d5d6f37eec35461ef1d
wajeht <58354193+wajeht@users.noreply.github.com>
2022-07-26 17:19:14 -0400

    feat: added a toast for random notification

---
+++ src/apps/ui/components/dashboard/Toast.vue
@@ -0,0 +1,35 @@
+<script setup>
+import useAppStore from '../../store/app.store.js';
+
+const appStore = useAppStore();
+
+const props = defineProps({
+  message: String,
+});
+</script>
+
+<template>
+  <div
+    style="
+      position: fixed;
+      z-index: 999999999999999999;
+      top: 80px;
+      right: 17px;
+      font-size: 0.7rem;
+      width: fit-content;
+    "
+    class="toast show align-items-center bg-success bg-opacity-75 text-white border-0"
+    role="alert"
+  >
+    <div class="d-flex">
+      <div class="toast-body">{{ message }}</div>
+      <button
+        @click="appStore.clearToast()"
+        type="button"
+        class="btn-close btn-close-white me-2 m-auto"
+        data-bs-dismiss="toast"
+        aria-label="Close"
+      ></button>
+    </div>
+  </div>
+</template>
