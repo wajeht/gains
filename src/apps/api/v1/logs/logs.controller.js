5faec06bff4644f45d5457709074ddbc8cf1face
Jaw <58354193+wajeht@users.noreply.github.com>
2022-07-29 16:02:05 -0400

    refactor: use full join instead of inner joined so we don't have to call DB twice (#134)
    refactor: use full join instaed of inner joined so we dont have to call db twice

--- src/apps/api/v1/logs/logs.controller.js
+++ src/apps/api/v1/logs/logs.controller.js
@@ -1,9 +1,25 @@
 import * as LogsQueries from './logs.queries.js';
+import { StatusCodes } from 'http-status-codes';
+import CustomError from '../../api.errors.js';
+import logger from '../../../../libs/logger.js';
 
+/**
+ * It creates a log for a user
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 export async function createLogs(req, res) {
   const body = req.body;
-  const logs = await LogsQueries.createLog(body);
-  res.json({
-    data: logs,
+  const created = await LogsQueries.createLog(body);
+
+  if (!created.length) throw new CustomError.BadRequestError(`Something went wrong while creating a log for for log id: ${body.user_id}!`); // prettier-ignore
+
+  logger.info(`user id: ${body.user_id} has created a log id: ${created[0].id}`);
+
+  res.status(StatusCodes.CREATED).json({
+    status: 'success',
+    request_url: req.originalUrl,
+    message: 'The resource was created successfully!',
+    data: created,
   });
 }
