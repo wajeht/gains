6dcd0c3929219ad3c766f4e2a1e078723bbe7a0c
wajeht <58354193+wajeht@users.noreply.github.com>
2022-07-28 23:47:15 -0400

    feat: change sessioins back to 20

--- src/apps/ui/App.vue
+++ src/apps/ui/App.vue
@@ -32,3 +32,36 @@ export default {
   },
 };
 </script>
+
+<style>
+.btn-ripple {
+  display: inline-block;
+  position: relative;
+  overflow: hidden;
+  transition: all ease-in-out 0.5s;
+}
+
+.btn-ripple::after {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 25%;
+  height: 100%;
+  width: 50%;
+  background-color: #000;
+  border-radius: 50%;
+  opacity: 0;
+  pointer-events: none;
+  transition: all ease-in-out 0.5s;
+  transform: scale(5, 5);
+}
+
+.btn-ripple:active::after {
+  padding: 0;
+  margin: 0;
+  opacity: 0.2;
+  transition: 0s;
+  transform: scale(0, 0);
+}
+</style>
