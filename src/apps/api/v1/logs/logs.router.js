2e9a5b131dc4a7fbd08764e95456e2c580311a1f
wajeht <58354193+wajeht@users.noreply.github.com>
2022-08-11 23:17:04 -0400

    chore: update api docs description

--- src/apps/api/v1/logs/logs.router.js
+++ src/apps/api/v1/logs/logs.router.js
@@ -19,16 +19,14 @@ const logs = express.Router();
  */
 logs.post('/', validator(LogsValidation.createLogs), catchAsyncErrors(LogsController.createLogs));
 
-// /**
-//  * POST /api/v1/logs/multiple
-//  * @tags logs
-//  * @summary create a log for a session
-//  * @param {string} name.form.required - the name of the log - application/x-www-form-urlencoded
-//  * @param {number} session_id.form.required - current session_id - application/x-www-form-urlencoded
-//  * @param {number} user_id.form.required  - current session_id - application/x-www-form-urlencoded
-//  * @param {string} notes.form.required- notes for current session - application/x-www-form-urlencoded
-//  * @param {json} json.form.required- - the json - application/x-www-form-urlencoded
-//  */
+/**
+ * POST /api/v1/logs/multiple
+ * @tags logs
+ * @summary create a multiple logs at once
+ * @param {number} session_id.form.required - current session_id - application/x-www-form-urlencoded
+ * @param {number} user_id.form.required  - current session_id - application/x-www-form-urlencoded
+ * @param {array} logs.form.required- - the logs - application/x-www-form-urlencoded
+ */
 logs.post(
   '/multiple',
   validator(LogsValidation.postMultipleLogs),
