a4098ea2d81f420c9b2f0ee411b688e5d9a67c00
wajeht <58354193+wajeht@users.noreply.github.com>
2022-08-13 01:50:38 -0400

    feat: added the ability to delete user accoutn via api/ui

--- src/apps/ui/pages/dashboard/settings/Settings.vue
+++ src/apps/ui/pages/dashboard/settings/Settings.vue
@@ -56,7 +56,10 @@ const appStore = useAppStore();
             </div>
 
             <!-- delete  -->
-            <router-link to="#" class="list-group-item list-group-item-action d-flex gap-3 py-3">
+            <router-link
+              to="/dashboard/settings/account/delete-account"
+              class="list-group-item list-group-item-action d-flex gap-3 py-3"
+            >
               <div class="d-flex gap-2 w-100 justify-content-between">
                 <div>
                   <h6 class="mb-0">
