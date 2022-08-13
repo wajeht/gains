415e57ceab5e89259bca3659741517571505b0bd
wajeht <58354193+wajeht@users.noreply.github.com>
2022-08-13 02:37:27 -0400

    Revert "feat: remove background scroll when modal is active (#166)"
    This reverts commit 34fa37a5e39a68c6432c7c00a0cbb6ce3d985403.

--- src/apps/ui/pages/dashboard/sessions/Exercises.vue
+++ src/apps/ui/pages/dashboard/sessions/Exercises.vue
@@ -232,7 +232,7 @@ function clearDataAndDismissModal() {
           data-bs-keyboard="false"
           tabindex="-1"
         >
-          <div class="modal-dialog">
+          <div class="modal-dialog modal-dialog-scrollable">
             <div class="modal-content">
               <div class="modal-header">
                 <h5 class="modal-title">Add a exercise</h5>
