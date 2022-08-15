906e3fed090141d8b9500b6aed5cdea92750430f
Jaw <58354193+wajeht@users.noreply.github.com>
2022-08-15 18:11:43 -0400

    feat: updated caprover script (#169)

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-caprover deploy --default
+source .env
+
+# caprover deploy --default # this only works if the project wasted deployed on current path
+
+caprover deploy -h $CAPROVER_DOMAIN -p $CAPROVER_PASSWORD -b $CAPROVER_GIT_BRANCH_NAME -a $CAPROVER_APP_NAME
