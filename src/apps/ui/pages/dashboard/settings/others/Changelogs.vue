8ea58152253aad2260e7509a38687cbaf40a389e
wajeht <58354193+wajeht@users.noreply.github.com>
2022-08-31 02:17:12 -0400

    feat: only show changeloag after api call

--- src/apps/ui/pages/dashboard/settings/others/Changelogs.vue
+++ src/apps/ui/pages/dashboard/settings/others/Changelogs.vue
@@ -1,32 +1,34 @@
 <script setup>
 import Backheader from '../../../../components/dashboard/headers/Backheader.vue';
-import api from '../../../../../../utils/fetch-with-style.js'
+import api from '../../../../../../utils/fetch-with-style.js';
 import useAppStore from '../../../../store/app.store';
-import { ref, onMounted } from 'vue'
+import { ref, onMounted } from 'vue';
 
-const appStore = useAppStore()
-const changelogs = ref('')
+const appStore = useAppStore();
+const changelogs = ref('');
 
 async function getChangelogs() {
-  const res = await api.get(`/api/v1/variables/changelogs`)
-  const json = await res.json()
-  return json.changelogs
+  const res = await api.get(`/api/v1/variables/changelogs`);
+  const json = await res.json();
+  return json.changelogs;
 }
 
 onMounted(async () => {
   appStore.loading = true;
-  const data = await getChangelogs()
-  changelogs.value = data
+  const data = await getChangelogs();
+  changelogs.value = data;
   appStore.loading = false;
 });
-
 </script>
 
 <template>
   <!-- header -->
   <Backheader />
 
-  <div class="container px-3 animate__animated animate__fadeIn animate__faster">
+  <div
+    v-if="!appStore.loading"
+    class="container px-3 animate__animated animate__fadeIn animate__faster"
+  >
     <div class="my-3 d-flex flex-column gap-3">
       <div class="card">
         <div class="card-body">
