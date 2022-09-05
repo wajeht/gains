d7dfe1f4f13338e4b01d74eefae160a444c69d86
wajeht <58354193+wajeht@users.noreply.github.com>
2022-09-05 11:31:48 -0400

    chore: added love footer

--- src/apps/ui/components/regular/RegularFooter.vue
+++ src/apps/ui/components/regular/RegularFooter.vue
@@ -2,18 +2,34 @@
   <footer class="py-4 border-top bg-white mt-4">
     <div class="container d-flex justify-content-between">
       <!-- copyright -->
-      <span class="text-muted">Copyright © 2022 allkindsofgains.app</span>
+      <div class="text-muted d-flex gap-2">
+        <span> Copyright © 2022 allkindsofgains.app. </span>
+        <span id="love"> Made with ❤️ by group of strength athletes! </span>
+      </div>
 
       <!-- social links -->
       <div class="d-flex gap-2">
         <!-- github -->
-        <a class="link-secondary" href="https://github.com/allkindsofgains" target="_blank"><i
-            class="bi bi-github"></i></a>
+        <a class="link-secondary" href="https://github.com/allkindsofgains" target="_blank"
+          ><i class="bi bi-github"></i
+        ></a>
 
         <!-- discord -->
-        <a class="link-secondary" href="https://discord.com/channels/988167900420534273/989628732895543296"
-          target="_blank"><i class="bi bi-discord"></i></a>
+        <a
+          class="link-secondary"
+          href="https://discord.com/channels/988167900420534273/989628732895543296"
+          target="_blank"
+          ><i class="bi bi-discord"></i
+        ></a>
       </div>
     </div>
   </footer>
 </template>
+
+<style scoped>
+@media screen and (max-width: 768px) {
+  #love {
+    display: none;
+  }
+}
+</style>
