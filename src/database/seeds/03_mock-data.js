ada8f20321c0e35254dd7cddba60886b8a18a571
wajeht <58354193+wajeht@users.noreply.github.com>
2022-09-09 02:59:55 -0400

    feat: added the ability to generate mock data in dev env

---
+++ src/database/seeds/03_mock-data.js
@@ -0,0 +1,91 @@
+import logger from '../../utils/logger.js';
+import Chad from '../../utils/chad.js';
+import redis from '../../utils/redis.js';
+import { sleep } from '../../utils/helpers.js';
+import { faker } from '@faker-js/faker';
+import chalk from 'chalk';
+import * as SessionsQueries from '../../apps/api/v1/sessions/sessions.queries.js';
+import * as CacheQueries from '../../apps/api/v1/cache/cache.queries.js';
+import * as UsersQueries from '../../apps/api/v1/users/users.queries.js';
+import * as LogsQueries from '../../apps/api/v1/logs/logs.queries.js';
+import * as ExercisesQueries from '../../apps/api/v1/exercises/exercises.queries.js';
+import * as SetsQueries from '../../apps/api/v1/sets/sets.queries.js';
+
+let [env_admin_user] = await UsersQueries.findUserByParam({ email: process.env.ADMIN_EMAIL });
+const user_id = process.argv[2] ?? env_admin_user.id;
+
+try {
+  // generate 20 sessions at a time
+  for (let k = 0; k < 20; k++) {
+    console.log('-'.repeat(process.stdout.columns));
+    // generate a session
+    const [session] = await SessionsQueries.createASession({
+      name: faker.lorem.words(5),
+      body_weight: faker.datatype.number({ max: 225 }),
+      caffeine_intake: faker.datatype.number({ max: 300 }),
+      calories_prior_session: faker.datatype.number({ max: 1500 }),
+      total_calories: faker.datatype.number({ max: 3000 }),
+      water_prior_session: faker.datatype.number({ max: 1000 }),
+      total_water: faker.datatype.number({ max: 1000 }),
+      hours_of_sleep: faker.datatype.number({ max: 8 }),
+      stress_level: faker.datatype.number({ max: 10 }),
+      notes: faker.lorem.words(10),
+      user_id: user_id,
+    });
+    console.log();
+    logger.info(`session: ${session.name}`);
+
+    // generate a exercise
+    const exercises = await ExercisesQueries.getExerciseByUserId(user_id);
+
+    // generate a log
+    for (let i = 0; i < faker.datatype.number({ max: 10 }); i++) {
+      const randomNumber = faker.datatype.number({ max: exercises.length - 1 });
+      const randomExercise = exercises[randomNumber];
+
+      logger.info(`lift: ${randomExercise.name}`);
+
+      const [log] = await LogsQueries.createLog({
+        name: randomExercise.name,
+        notes: faker.lorem.words(10),
+        user_id: user_id,
+        session_id: session.id,
+        exercise_id: randomExercise.id,
+        collapsed: true,
+        private: false,
+        sets_notes_visibility: true,
+      });
+
+      // logger.info(`generating a log for exercise: ${log}`);
+
+      // generate sets for log above
+      for (let j = 0; j < faker.datatype.number({ max: 10 }); j++) {
+        const [set] = await SetsQueries.createSet({
+          log_id: log.id,
+          user_id: log.user_id,
+          exercise_id: log.exercise_id,
+          session_id: log.session_id,
+          notes: faker.lorem.words(10),
+          reps: faker.datatype.number({ max: 15 }),
+          weight: faker.datatype.number({ max: 500 }),
+          rpe: faker.datatype.number({ max: 10 }),
+        });
+
+        logger.info(
+          ` - ${set.reps} x ${set.weight} @${set.rpe} - ${set.notes
+            .split(' ')
+            .slice(0, 3)
+            .join(' ')}...`,
+        );
+      }
+      console.log();
+    }
+
+    // clear all the cache
+    await redis.flushall();
+  }
+  process.exit();
+} catch (e) {
+  logger.error(e);
+  process.exit();
+}
