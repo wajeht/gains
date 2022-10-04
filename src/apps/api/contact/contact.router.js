fb6eec2d8ee002ef35d7d0f9a11c5acd0ebf1c93
wajeht <58354193+wajeht@users.noreply.github.com>
2022-10-04 19:54:15 -0400

    feat: added swagger api docs for contact route

--- src/apps/api/contact/contact.router.js
+++ src/apps/api/contact/contact.router.js
@@ -6,6 +6,14 @@ import * as ContactValidation from './contact.validation.js';
 import express from 'express';
 const contact = express.Router();
 
+/**
+ * POST /api/contact
+ * @tags contact
+ * @summary create a contact
+ * @param {string} email.form.required - the email - application/x-www-form-urlencoded
+ * @param {string} subject.form.required - the subject - application/x-www-form-urlencoded
+ * @param {string} message.form.required - the message - application/x-www-form-urlencoded
+ */
 contact.post(
   '/',
   validator(ContactValidation.postContact),
