b457e873d248025d17648c2957556d0e2221aa2e
wajeht <58354193+wajeht@users.noreply.github.com>
2022-10-08 18:57:43 -0400

    feat: scaffold users table for admin panel --no-verify

--- src/apps/ui/pages/admin/AdminUsers.vue
+++ src/apps/ui/pages/admin/AdminUsers.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted, reactive } from 'vue';
 import useAppStore from '../../store/app.store.js';
 import api from '../../../../utils/fetch-with-style.js';
+import dayjs from 'dayjs';
 
 const appStore = useAppStore();
 const alert = reactive({ type: '', msg: '' });
@@ -45,16 +46,64 @@ async function fetchUsers() {
   </div>
 
   <div class="card">
+    <div class="card-header">Featured</div>
     <div class="card-body">
-      <h5 class="card-title">Admin Users</h5>
-      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-      <p class="card-text">
-        <pre>
-        {{ users }}
-        </pre>
-      </p>
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">User</th>
+            <th scope="col">Date Created</th>
+            <th scope="col">Verified</th>
+            <th scope="col">Deleted</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="u in users"
+            :key="`user-key-${u.id}`"
+            :class="{ 'grayscale text-muted': !u.verified || u.deleted }"
+          >
+            <th scope="row">{{ u.id }}</th>
+            <td>
+              <div class="d-flex gap-1">
+                <!-- pic -->
+                <img :src="u.profile_picture_url" style="max-width: 50px; max-height: auto" />
+
+                <!-- role -->
+                <div class="d-flex flex-column gap-1">
+                  <span class="d-flex gap-1 justify-content-center align-items-center">
+                    <span class="fw-bold">{{ u.first_name + ' ' + u.last_name }}</span>
+                    <small class="fst-italic"> - {{ u.role }}</small>
+                  </span>
+                  <small class="d-flex flex-column">
+                    <span>@{{ u.username }}</span>
+                    <span>{{ u.weight }} {{ appStore.unit.label }}</span>
+                    <span>{{ dayjs(u.birth_date).format('YYYY/MM/DD') }}</span>
+                  </small>
+                </div>
+              </div>
+            </td>
+            <td>{{ dayjs(u.created_at).format('YYYY/MM/DD') }}</td>
+            <td>{{ u.verified }}</td>
+            <td>{{ u.deleted }}</td>
+            <td>
+              <div class="d-flex gap-2">
+                <span role="button"><i class="bi bi-pencil-square"></i></span>
+                <span role="button"><i class="bi bi-trash"></i></span>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+    <div class="card-footer text-muted">2 days ago</div>
   </div>
 </template>
+
+<style scoped>
+.grayscale {
+  filter: grayscale(100);
+}
+</style>
