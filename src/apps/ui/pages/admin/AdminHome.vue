522eb0cea1ade9d23d03dbe4a345a5f8a8f31492
wajeht <58354193+wajeht@users.noreply.github.com>
2022-10-07 23:23:42 -0400

    feat: scaffold menu links for admin ui

--- src/apps/ui/pages/admin/AdminHome.vue
+++ src/apps/ui/pages/admin/AdminHome.vue
@@ -1,5 +1,38 @@
 <script setup></script>
 
-<template>admin home</template>
+<template>
+  <div class="px-4 animate__animated animate__fadeIn">
+    <div class="row gap-3">
+      <!-- menu -->
+      <div class="col-sm-12 col-lg-3">
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+            The current link item
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+          <a href="#" class="list-group-item list-group-item-action">A third link item</a>
+          <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+          <a class="list-group-item list-group-item-action disabled">A disabled link item</a>
+        </div>
+      </div>
+
+      <!-- card -->
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the bulk of the card's
+              content.
+            </p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style></style>
