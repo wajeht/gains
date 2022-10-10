2fff2271bd44982efa6632abdfec3bff18f6ebf2
Jaw <58354193+wajeht@users.noreply.github.com>
2022-10-10 15:48:41 -0400

    feat: added a inside loading component and fix a bunch of user admin table style related issues

---
+++ src/apps/ui/components/shared/InsideLoading.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    id="inside-loading-spinner"
+    class="d-flex justify-content-center align-items-center container bg-white"
+    style="width: 100%; height: 100%; position: absolute; z-index: 10000; opacity: 0.7"
+  >
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
--- src/apps/ui/pages/admin/AdminUsers.vue
+++ src/apps/ui/pages/admin/AdminUsers.vue
@@ -3,13 +3,21 @@ import { ref, onMounted, reactive } from 'vue';
 import useAppStore from '../../store/app.store.js';
 import api from '../../../../utils/fetch-with-style.js';
 import dayjs from 'dayjs';
+import { sleep } from '../../../../utils/helpers.js';
+import InsideLoading from '../../components/shared/InsideLoading.vue';
 
 const appStore = useAppStore();
 const alert = reactive({ type: '', msg: '' });
 const users = ref([]);
+const loading = ref(false);
 
 onMounted(async () => {
+  loading.value = true;
+
+  await sleep(1000);
   await fetchUsers();
+
+  loading.value = false;
 });
 
 async function fetchUsers() {
@@ -45,110 +53,135 @@ async function fetchUsers() {
     <span>{{ alert.msg }}</span>
   </div>
 
-  <div class="card">
-    <div class="card-header">Featured</div>
-    <div class="card-body">
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">User</th>
-            <th scope="col">Date Created</th>
-            <th scope="col">Status</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="u in users" :key="`user-key-${u.id}`">
-            <!-- id -->
-            <th scope="row">{{ u.id }}</th>
+  <div style="position: relative">
+    <!-- loading -->
+    <InsideLoading v-if="loading" />
 
-            <!-- user -->
-            <td>
-              <div class="d-flex gap-1">
-                <!-- pic -->
-                <img
-                  :src="u.profile_picture_url"
-                  style="max-width: 20%; max-height: auto; box-sizing: border-box"
-                  :style="{
-                    'border-left': u.role === 'admin' ? '5px solid #FAA819 !important' : '',
-                  }"
-                />
+    <!-- card -->
+    <div class="card">
+      <!-- header -->
+      <div class="card-header">Featured</div>
 
-                <!-- role -->
-                <div class="d-flex flex-column gap-1">
-                  <span class="d-flex gap-1 justify-content-center align-items-center">
-                    <span class="fw-bold">{{ u.first_name + ' ' + u.last_name }}</span>
-                    <small class="fst-italic fw-light">
-                      - {{ u.role }}
-                      <font-awesome-icon
-                        v-if="u.role === 'admin'"
-                        icon="fa-crown"
-                        style="color: #faa819"
-                    /></small>
-                  </span>
-                  <small class="d-flex flex-column fw-light">
-                    <!-- username -->
-                    <span><i class="bi bi-person-fill me-1"></i>@{{ u.username }}</span>
+      <!-- body -->
+      <div class="card-body" style="overflow: scroll !important">
+        <table class="table text-nowrap">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">User</th>
+              <th scope="col">Date Created</th>
+              <th scope="col">Status</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="u in users" :key="`user-key-${u.id}`">
+              <!-- id -->
+              <th scope="row">{{ u.id }}</th>
 
-                    <!-- weight -->
-                    <span v-if="u.weight">
-                      <font-awesome-icon icon="fa-weight-scale " class="me-1" />{{ u.weight }}
-                      {{ appStore.unit.label }}
+              <!-- user -->
+              <td>
+                <div class="d-flex gap-1">
+                  <!-- pic -->
+                  <img
+                    :src="u.profile_picture_url"
+                    style="max-width: 20%; max-height: auto; box-sizing: border-box"
+                    :style="{
+                      'border-left': u.role === 'admin' ? '5px solid #FAA819 !important' : '',
+                    }"
+                  />
+
+                  <!-- role -->
+                  <div class="d-flex flex-column gap-1">
+                    <span class="d-flex gap-1 justify-content-center align-items-center">
+                      <span class="fw-bold">{{ u.first_name + ' ' + u.last_name }}</span>
+                      <small class="fst-italic fw-light">
+                        - {{ u.role }}
+                        <font-awesome-icon
+                          v-if="u.role === 'admin'"
+                          icon="fa-crown"
+                          style="color: #faa819"
+                      /></small>
                     </span>
+                    <small class="d-flex flex-column fw-light">
+                      <!-- username -->
+                      <span><i class="bi bi-person-fill me-1"></i>@{{ u.username }}</span>
 
-                    <!-- birthday -->
-                    <span v-if="u.birth_date">
-                      <i class="bi bi-balloon-fill me-1"></i
-                      >{{ dayjs(u.birth_date).format('YYYY/MM/DD') }}</span
-                    >
+                      <!-- weight -->
+                      <span v-if="u.weight">
+                        <font-awesome-icon icon="fa-weight-scale " class="me-1" />{{ u.weight }}
+                        {{ appStore.unit.label }}
+                      </span>
+
+                      <!-- birthday -->
+                      <span v-if="u.birth_date">
+                        <i class="bi bi-balloon-fill me-1"></i
+                        >{{ dayjs(u.birth_date).format('YYYY/MM/DD') }}</span
+                      >
+                    </small>
+                  </div>
+                </div>
+              </td>
+
+              <!-- date created -->
+              <td>{{ dayjs(u.created_at).format('YYYY/MM/DD') }}</td>
+
+              <!-- status -->
+              <td>
+                <div class="d-flex flex-column gap-1">
+                  <!-- verified -->
+                  <small
+                    :class="{
+                      'bg-success text-white px-1 rounded': u.verified,
+                      'bg-danger text-white px-1 rounded': !u.verified,
+                    }"
+                  >
+                    <span v-if="!u.verified">not verified</span>
+                    <span v-if="u.verified">verified</span>
+                  </small>
+
+                  <!-- deleted -->
+                  <small
+                    :class="{
+                      'bg-success text-white px-1 rounded': !u.deleted,
+                      'bg-danger text-white px-1 rounded': u.deleted,
+                    }"
+                  >
+                    <span v-if="!u.deleted">active</span>
+                    <span v-if="u.deleted">inactive</span>
                   </small>
                 </div>
-              </div>
-            </td>
+              </td>
 
-            <!-- date created -->
-            <td>{{ dayjs(u.created_at).format('YYYY/MM/DD') }}</td>
+              <!-- actions -->
+              <td>
+                <div class="d-flex gap-2">
+                  <span role="button"><i class="bi bi-pencil-square"></i></span>
+                  <span role="button"><i class="bi bi-trash"></i></span>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-            <!-- status -->
-            <td>
-              <div class="d-flex flex-column gap-1">
-                <!-- verified -->
-                <span
-                  :class="{
-                    'bg-success text-white px-1 rounded': u.verified,
-                    'bg-danger text-white px-1 rounded': !u.verified,
-                  }"
-                >
-                  <span v-if="!u.verified">not verified</span>
-                  <span v-if="u.verified">verified</span>
-                </span>
-
-                <!-- deleted -->
-                <span
-                  :class="{
-                    'bg-success text-white px-1 rounded': !u.deleted,
-                    'bg-danger text-white px-1 rounded': u.deleted,
-                  }"
-                >
-                  <span v-if="!u.deleted">active</span>
-                  <span v-if="u.deleted">inactive</span>
-                </span>
-              </div>
-            </td>
-
-            <!-- actions -->
-            <td>
-              <div class="d-flex gap-2">
-                <span role="button"><i class="bi bi-pencil-square"></i></span>
-                <span role="button"><i class="bi bi-trash"></i></span>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <!-- footer -->
+      <div class="card-footer text-muted">
+        <nav aria-label="Page navigation example" class="py-2">
+          <ul class="pagination justify-content-center mb-0 pb-0">
+            <li class="page-item disabled">
+              <a class="page-link">Previous</a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+              <a class="page-link" href="#">Next</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
-    <div class="card-footer text-muted">2 days ago</div>
   </div>
 </template>
 
