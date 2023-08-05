b0b0365feade832384e50b0f53aa1783d16c3b42
Jaw <58354193+wajeht@users.noreply.github.com>
2023-08-05 04:50:34 -0500

    feat: handle graceful shutdown

--- src/apps/server.js
+++ src/apps/server.js
@@ -11,11 +11,38 @@ import pkg from '../utils/pkg.js';
 import EmailService from '../services/email.service.js';
 import latestChangelog from '../utils/latest-changelog.js';
 import CronsServices from '../services/cron.services.js';
+import redis from '../utils/redis.js';
 
 app.listen(port, () => {
   logger.warn(`Server is on ${env} mode!`);
   logger.info(`Server is running on http://localhost:${port}!`);
   if (process.env.HMR === 'true') logger.warn(`But use http://localhost:${vue_port} for hmr!`);
+});
+
+async function gracefulShutdown() {
+  logger.info('**** Received kill signal, shutting down gracefully. ****');
+  server.close(async () => {
+    try {
+      await redis.disconnect();
+      await db.destroy();
+      logger.info('**** Closed out remaining connections. ****');
+      process.exit();
+    } catch (err) {
+      logger.error('**** Error during shutdown ', err, ' ****');
+      process.exit(1);
+    }
+  });
+
+  setTimeout(() => {
+    logger.error('**** Could not close connections in time, forcefully shutting down ****');
+    process.exit(1);
+  }, 30 * 1000); // force shutdown after 30s
+}
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error('**** Unhandled Rejection at: ', promise, ' reason: ', reason, ' ****');
 });
 
 // ------------------------------ auto migrate db on start (production only) ------------------------------
