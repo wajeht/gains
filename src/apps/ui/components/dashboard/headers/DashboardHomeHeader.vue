053a0f6d9e2aee427f05cbd4ff1e5436610d59a0
wajeht <58354193+wajeht@users.noreply.github.com>
2023-08-17 18:08:50 -0500

    feat: enable chat route

--- src/apps/ui/components/dashboard/headers/DashboardHomeHeader.vue
+++ src/apps/ui/components/dashboard/headers/DashboardHomeHeader.vue
@@ -11,50 +11,13 @@ const userStore = useUserStore();
 
 const loading = ref(false);
 
-async function logout() {
-  try {
-    loading.value = true;
-
-    const res = await api.get('/api/auth/logout');
-    const json = await res.json();
-
-    if (!res.ok) {
-      loading.value = false;
-      if (json.errors) {
-        throw json.errors;
-      } else {
-        throw json.message;
-      }
-    }
-
-    userStore.isLoggedIn = false;
-    userStore.clearUserInfo();
-
-    let logoutLink = '/login';
-    if (isMobile()) {
-      logoutLink = '/dashboard/login';
-    }
-
-    router.push({ path: logoutLink });
-  } catch (e) {
-    loading.value = false;
-    alert.type = 'danger';
-    if (Array.isArray(e)) {
-      alert.msg = e.map((cur) => cur.msg).join(' ');
-      return;
-    } else {
-      alert.msg = e;
-    }
-  }
-}
 </script>
 
 <template>
   <div
     id="dashboard-home-header"
     style="height: 64px"
-    class="container sticky-top d-flex justify-content-between align-items-center bg-white border-bottom py-3 gap-3"
-  >
+    class="container sticky-top d-flex justify-content-between align-items-center bg-white border-bottom py-3 gap-3">
     <!-- profile view -->
     <router-link to="/dashboard/profile" class="link-secondary" role="button">
       <h5 class="m-0 p-0 d-flex justify-content-center align-items-center gap-2">
@@ -63,17 +26,10 @@ async function logout() {
       </h5>
     </router-link>
 
-    <!-- logout -->
-    <button @click="logout()" class="btn btn-danger" :disabled="loading">
-      <div v-if="loading" class="spinner-border spinner-border-sm" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <span v-if="!loading">
-        <font-awesome-icon icon="right-from-bracket" />
-        Logout
-      </span>
-      <span v-if="loading"> Loading... </span>
-    </button>
+    <!-- chat -->
+    <router-link to="/dashboard/chat">
+      <i class="bi bi-chat" style="font-size: 20px"></i>
+    </router-link>
   </div>
 </template>
 
