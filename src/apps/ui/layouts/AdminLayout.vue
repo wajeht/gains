c8663e32e57f7bd2aafddf9e3f8cf161a478c1dd
Jaw <58354193+wajeht@users.noreply.github.com>
2023-08-19 03:17:44 -0500

    admins tuff

--- src/apps/ui/layouts/AdminLayout.vue
+++ src/apps/ui/layouts/AdminLayout.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { reactive } from 'vue';
 import RegularHeader from '../components/regular/RegularHeader.vue';
 import RegularFooter from '../components/regular/RegularFooter.vue';
 import ActivityLog from '../components/admin/ActivityLog.vue';
@@ -10,12 +11,38 @@ import api from '../../../utils/fetch-with-style.js';
 
 const appStore = useAppStore();
 
+const states = reactive({
+  alert: {
+    msg: '',
+    type: '',
+  },
+  loading: false,
+});
+
 async function clearAllCache() {
   try {
+    states.loading = true;
     await api.get('/api/admin/clear-all-cache');
-  } catch (error) {
-    console.error(error);
+    states.alert.type = 'success';
+    states.alert.msg = 'All cache in the systems has been cleared!';
+
+    setTimeout(() => {
+      states.alert.type = '';
+      states.alert.msg = '';
+    }, 5000);
+  } catch (e) {
+    states.loading = false;
+    states.alert.type = 'danger';
+    if (Array.isArray(e)) {
+      states.alert.msg = e.map((cur) => cur.msg).join(' ');
+      return;
+    } else {
+      states.alert.msg = e;
+    }
+  } finally {
+    states.loading = false;
   }
+
 }
 </script>
 
@@ -34,8 +61,14 @@ async function clearAllCache() {
           <OnlineUsers />
 
           <!-- clear all cache -->
-          <button class="btn btn-dark" style="min-height: 43px !important" @click="clearAllCache">
-            Clear All Cache
+          <button class="btn btn-primary" :disabled="states.loading" style="min-height: 43px !important"
+            @click="clearAllCache">
+            <div v-if="states.loading" class="spinner-border spinner-border-sm" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+
+            <span v-if="!states.loading"> Clear All Cache </span>
+            <span v-if="states.loading"> Loading... </span>
           </button>
 
           <!-- logout -->
@@ -44,6 +77,12 @@ async function clearAllCache() {
 
         <!-- card -->
         <div class="col d-flex flex-column gap-4">
+
+          <!-- alert -->
+          <div v-if="states.alert.type" :class="`alert-${states.alert.type}`" class="alert" style="margin-bottom: -10px;">
+            <span>{{ states.alert.msg }}</span>
+          </div>
+
           <!-- admin pages -->
           <router-view> </router-view>
 
