e469c05c3af852ae000d71b7e8d8b5d68c7f841a
Jaw <58354193+wajeht@users.noreply.github.com>
2023-08-20 18:35:36 -0500

    feat: use real profile image for regular search stuff

--- src/apps/ui/components/dashboard/DashboardHeader.vue
+++ src/apps/ui/components/dashboard/DashboardHeader.vue
@@ -1,15 +1,24 @@
+<script setup>
+import { computed } from 'vue';
+import useUserStore from '../../store/user.store';
+const userStore = useUserStore();
+
+const computedProfileImage = computed(() => {
+  return userStore.user.profile_picture_url ?? `https://dummyimage.com/200x200/bdbdbd/000000.jpg`;
+});
+</script>
 <template>
   <div
     id="dashboard-header"
     class="container sticky-top d-flex justify-content-between align-items-center bg-white border-bottom py-3 gap-3"
   >
     <!-- profile image -->
-    <router-link :class="{ active: $route.name === 'Profile' }" to="/dashboard/profile">
+    <router-link :class="{ active: $route.name === 'Profile' }" :to="`/dashboard/profile/${userStore.user.username}`">
       <img
         class="rounded"
         width="25"
         height="25"
-        src="../../assets/images/logo.png"
+        :src="computedProfileImage"
         alt="profile-image"
       />
     </router-link>
