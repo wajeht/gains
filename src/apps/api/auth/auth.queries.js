f0cd1d80474afbf1965d8ea33ed5bf5851ea779f
Jaw <58354193+wajeht@users.noreply.github.com>
2023-08-23 17:58:02 -0500

    chore: remove un-used comments

--- src/apps/api/auth/auth.queries.js
+++ src/apps/api/auth/auth.queries.js
@@ -1,11 +1,5 @@
 import db from '../../../database/db.js';
 
-/**
- * It updates the verified column of the user_details table to true where the user_id is equal to
- * the uid passed in as a parameter
- * @param uid - The user id of the user to be verified.
- * @returns A promise
- */
 export async function verifyUser(uid, date) {
   return db
     .update({ verified: true, verified_at: date })
@@ -14,13 +8,6 @@ export async function verifyUser(uid, date) {
     .returning('*');
 }
 
-/**
- * Update the password reset token and expiration for the user with the given uid.
- * @param uid - The user's ID
- * @param token - The token that will be used to reset the password.
- * @param expiration - The expiration date of the token.
- * @returns The user's password reset token and expiration date.
- */
 export async function generatePasswordResetToken(uid, token, expiration) {
   return db
     .update({
