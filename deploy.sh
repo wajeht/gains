c70dd1c8c6a5e546cc729467bf5a68ed77785ad8
wajeht <58354193+wajeht@users.noreply.github.com>
2023-09-19 21:59:29 -0500

    feat: desc timeout

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@
 
 source .env
 
-# caprover deploy --default # this only works if the project wasted deployed on current path
-
 caprover deploy -h $CAPROVER_DOMAIN -p $CAPROVER_PASSWORD -b $CAPROVER_GIT_BRANCH_NAME -a $CAPROVER_APP_NAME
