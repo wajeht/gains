2fd65bdee9154519df8414dc751aacb2acf16e11
wajeht <58354193+wajeht@users.noreply.github.com>
2024-08-31 01:10:25 -0500

    refactor(utils): Simplify message sending logic in Chad flex method

--- src/utils/chad.js
+++ src/utils/chad.js
@@ -1,36 +1,24 @@
 import logger from './logger.js';
-import { env } from '../config/env.js';
+import { env, notify } from '../config/env.js';
 
 export default class Chad {
   static async flex(msg, object = null) {
     try {
-      let params;
 
-      // use different format to send if we have object passed in
-      if (object == null) {
-        params = { username: 'Chad', content: msg };
-      } else {
-        params = {
-          username: 'Chad',
-          content: msg,
-          embeds: [
-            {
-              title: msg,
-              description: object,
-            },
-          ],
-        };
-      }
-
-      if (env === 'production') {
+      if (env !== 'production') {
         const res = await fetch(notify.url, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
             'X-API-KEY': notify.xApiKey,
           },
-          body: JSON.stringify(params),
+          body: JSON.stringify({
+            message: msg,
+            details: object,
+          }),
         });
+
+        console.log(await res.json());
 
         if (res.ok) {
           logger.info(`Chad sent ${msg}`);
