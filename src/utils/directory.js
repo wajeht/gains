860cde8618e9bb9434eeabfc877a79243a9c7cc0
wajeht <58354193+wajeht@users.noreply.github.com>
2026-01-11 23:46:39 -0600

    chore: update path to kill-used-port script

--- src/utils/directory.js
+++ src/utils/directory.js
@@ -1,6 +1,5 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-// export const root = path.resolve(path.join(process.cwd()));
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 export const root = path.resolve(path.join(__dirname, '../../'));
